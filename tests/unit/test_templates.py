4dc929ffd2451269dee2015013dab73060a42eb8
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-02-10 09:10:58 +0100

    templates: add unit tests
    * Add unit tests to validate that each template has valid Jinja2 syntax.
    
    Bug: T244690
    Change-Id: Ie2f4762e9f1bfc0c26ba1ff91b9c6429d35cb803

---
+++ tests/unit/test_templates.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import pytest
+
+from jinja2 import Template
+
+
+TEMPLATE_DIR = 'templates'
+
+
+def get_templates():
+    path = Path(TEMPLATE_DIR)
+    return list(path.glob('**/*.conf'))
+
+
+@pytest.mark.parametrize('template', get_templates())
+def test_valid_jinja2(template):
+    with open(str(template)) as f:
+        Template(f.read())  # If invalid will raise
