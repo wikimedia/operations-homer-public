24569ef465f6df23e6bcacba0362110e688fb6f5
Arzhel Younsi <ayounsi@wikimedia.org>
2025-08-26 15:45:29 +0200

    Format existing python files using black and isort
    Change-Id: I97f1ce90d57ae129f6225d2608bac847617e1aca

--- tests/generate_schema_docs.py
+++ tests/generate_schema_docs.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import re
-import subprocess
 import shutil
+import subprocess
 from html.parser import HTMLParser
 from pathlib import Path
 
 import requests
 
-
-INDEX_PREFIX = """<!DOCTYPE html>
+INDEX_PREFIX: str = """<!DOCTYPE html>
 <html>
   <head>
     <title>Homer Public documentation</title>
@@ -32,41 +31,43 @@ class ExternalLinksParser(HTMLParser):
     def __init__(self, *args, **kwargs):
         """Initialize the instance."""
         self.links = {}
-        self.base_path = kwargs['output_base_path']
-        del kwargs['output_base_path']
+        self.base_path = kwargs["output_base_path"]
+        del kwargs["output_base_path"]
         super().__init__(*args, **kwargs)
 
     def handle_starttag(self, tag, attrs):
         """Gets called when an HTML tag is opened."""
-        if tag == 'link':
-            self._process_tag('href', attrs)
+        if tag == "link":
+            self._process_tag("href", attrs)
 
-        if tag == 'script':
-            self._process_tag('src', attrs)
+        if tag == "script":
+            self._process_tag("src", attrs)
 
     def _process_tag(self, key_name, attrs):
         """Process a given tag finding the external link."""
         for key, value in attrs:
             if key == key_name:
-                if value not in self.links and value.startswith('http'):
+                if value not in self.links and value.startswith("http"):
                     self._download_resource(key_name, value)
                 break
 
     def _download_resource(self, key, url):
         """Dowload the external resource in the base path."""
-        if url.startswith('https://use.fontawesome.com/'):  # Remove them completely
-            self.links[url] = ''
+        if url.startswith("https://use.fontawesome.com/"):  # Remove them completely
+            self.links[url] = ""
             return
 
         url_path = Path(requests.utils.urlparse(url).path)
         if not url_path.suffix:
-            url_path = url_path.with_suffix('.js' if key == 'src' else '.css')
+            url_path = url_path.with_suffix(".js" if key == "src" else ".css")
 
         file_content = requests.get(url, timeout=10).text
         file_path = self.base_path / url_path.name
         file_path.write_text(file_content)
 
-        if url_path.suffix == '.css' and 'src: url(' in file_content:  # Download the font
+        if (
+            url_path.suffix == ".css" and "src: url(" in file_content
+        ):  # Download the font
             links = self._download_fonts(file_content)
             replace_references(file_path, links)
 
@@ -76,9 +77,9 @@ class ExternalLinksParser(HTMLParser):
         """Download external fonts and return the links to replace."""
         links = {}
         for line in file_content.splitlines():
-            match = re.search(r'url\((?P<url>http[^)]+)\)', line)
+            match = re.search(r"url\((?P<url>http[^)]+)\)", line)
             if match is not None:
-                url = match.groupdict()['url']
+                url = match.groupdict()["url"]
                 name = Path(requests.utils.urlparse(url).path).name
                 (self.base_path / name).write_text(requests.get(url, timeout=10).text)
                 links[url] = name
@@ -90,7 +91,7 @@ def collect_references(file_path):
     """Collect all exteral references in the generated file and download them."""
     parser = ExternalLinksParser(output_base_path=file_path.parent)
     parser.feed(file_path.read_text())
-    print(f'Collected {len(parser.links)} external links')
+    print(f"Collected {len(parser.links)} external links")
     return parser.links
 
 
@@ -99,36 +100,40 @@ def replace_references(file_path, replaces):
     replaced = file_path.read_text()
     for find, replace in replaces.items():
         if not replace:  # Remove it completely
-            replaced = replaced.replace(f'<script src={find}></script>', '')
+            replaced = replaced.replace(f"<script src={find}></script>", "")
         else:
             replaced = replaced.replace(find, replace)
 
-    print(f'Replaced external links in {file_path}')
+    print(f"Replaced external links in {file_path}")
     file_path.write_text(replaced)
 
 
 def main():
     """Generate the documentation."""
-    output_path = Path('doc/build')
-    source_files = list(Path('tests/schemas/').glob('*.schema'))
+    output_path = Path("doc/build")
+    source_files = list(Path("tests/schemas/").glob("*.schema"))
 
     shutil.rmtree(output_path, ignore_errors=True)
     output_path.mkdir(mode=0o755, parents=True, exist_ok=True)
-    print('Reset build directory')
+    print("Reset build directory")
 
     links = []
     for schema in source_files:
-        print(f'Generating documentation for schema {schema}')
-        schema_output = output_path / f'{schema.name}.html'
-        subprocess.run(['generate-schema-doc', schema, schema_output], check=True)
-        links.append(f'<li><p><a href="{schema.name}.html">{schema.with_suffix(".yaml").name}</a></p></li>')
+        print(f"Generating documentation for schema {schema}")
+        schema_output = output_path / f"{schema.name}.html"
+        subprocess.run(["generate-schema-doc", schema, schema_output], check=True)
+        links.append(
+            f'<li><p><a href="{schema.name}.html">{schema.with_suffix(".yaml").name}</a></p></li>'
+        )
 
-    replaces = collect_references(output_path / f'{source_files[0].name}.html')
+    replaces = collect_references(output_path / f"{source_files[0].name}.html")
     for schema in source_files:
-        replace_references(output_path / f'{schema.name}.html', replaces)
+        replace_references(output_path / f"{schema.name}.html", replaces)
 
-    (output_path / 'index.html').write_text(INDEX_PREFIX + '\n'.join(links) + INDEX_SUFFIX)
+    (output_path / "index.html").write_text(
+        INDEX_PREFIX + "\n".join(links) + INDEX_SUFFIX
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,39 +1,37 @@
 import ipaddress
 import json
-
 from pathlib import Path
 
 import jsonschema
 import pytest
 import yaml
 
-
-CONFIG_DIR = Path('config')
-SCHEMA_DIR = Path('tests/schemas')
-DEFINITIONS_PATH = 'definitions'
+CONFIG_DIR = Path("config")
+SCHEMA_DIR = Path("tests/schemas")
+DEFINITIONS_PATH: str = "definitions"
 DEFINITIONS_DIR = SCHEMA_DIR / DEFINITIONS_PATH
 
 
-@pytest.fixture(name='jsonschema_store', scope='session')
+@pytest.fixture(name="jsonschema_store", scope="session")
 def _jsonschema_store():
     """Load the definitions used by $ref in the json schemas to be used by the RefResolver."""
     store = {}
-    for schema_file in DEFINITIONS_DIR.glob('*.schema'):
+    for schema_file in DEFINITIONS_DIR.glob("*.schema"):
         with open(schema_file) as f:
             schema = json.load(f)
-            store['/'.join((DEFINITIONS_PATH, schema['$id']))] = schema
+            store["/".join((DEFINITIONS_PATH, schema["$id"]))] = schema
 
     return store
 
 
-@jsonschema.FormatChecker.cls_checks('ip_network', raises=(ValueError,))
+@jsonschema.FormatChecker.cls_checks("ip_network", raises=(ValueError,))
 def is_ip_network(instance):
     """Custom jsonschema format validator for IPv4 and IPv6 networks."""
     ipaddress.ip_network(instance)  # Raises ValueError if invalid
     return True
 
 
-@jsonschema.FormatChecker.cls_checks('ip_interface', raises=(ValueError,))
+@jsonschema.FormatChecker.cls_checks("ip_interface", raises=(ValueError,))
 def is_ip_interface(instance):
     """Custom jsonschema format validator for IPv4 and IPv6 interfaces."""
     ipaddress.ip_interface(instance)  # Raises ValueError if invalid
@@ -42,22 +40,26 @@ def is_ip_interface(instance):
 
 def get_configs():
     """Collect all the existing config files and return them as a list of Paths."""
-    return list(CONFIG_DIR.glob('**/*.yaml'))
+    return list(CONFIG_DIR.glob("**/*.yaml"))
 
 
-@pytest.mark.parametrize('config_file', get_configs())
+@pytest.mark.parametrize("config_file", get_configs())
 def test_valid_config(jsonschema_store, config_file):
     """All config files should comply to their related schema."""
     config = yaml.safe_load(config_file.read_text())
-    schema_name = config_file.with_suffix('.schema').name
+    schema_name = config_file.with_suffix(".schema").name
     schema_file = SCHEMA_DIR / schema_name
     with open(schema_file) as f:
         schema = json.load(f)
 
-    resolver = jsonschema.RefResolver(base_uri=DEFINITIONS_PATH, referrer=schema, store=jsonschema_store)
+    resolver = jsonschema.RefResolver(
+        base_uri=DEFINITIONS_PATH, referrer=schema, store=jsonschema_store
+    )
     try:
         jsonschema.validate(instance=config, schema=schema, resolver=resolver)
     except jsonschema.exceptions.ValidationError:
-        print(f'Configuration file {config_file} failed validation, see '
-              f'https://doc.wikimedia.org/homer-public/master/{schema_name}.html')
+        print(
+            f"Configuration file {config_file} failed validation, see "
+            f"https://doc.wikimedia.org/homer-public/master/{schema_name}.html"
+        )
         raise
